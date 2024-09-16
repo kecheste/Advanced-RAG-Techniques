b68d084dcadaa529b909476f4a02d11044ed04da
Abenezer <aben.ezer443@gmail.com>
2024-09-16 14:51:37 +0300

    updated the notebooks

--- scripts/sentence_window_retrieval.py
+++ scripts/sentence_window_retrieval.py
@@ -1,31 +1,47 @@
 import os
-from llama_index import (
-    SimpleDirectoryReader,
-    Document,
-    StorageContext,
-    load_index_from_storage
+import openai
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core import Settings
+from llama_index.core import VectorStoreIndex
+from llama_index.core.postprocessor import MetadataReplacementPostProcessor
+from llama_index.core.evaluation import DatasetGenerator, QueryResponseDataset
+
+from llama_index.llms.openai import OpenAI
+import nest_asyncio
+import random
+import asyncio
+import numpy as np
+
+from llama_index.core.evaluation import (
+    CorrectnessEvaluator,
+    SemanticSimilarityEvaluator,
+    RelevancyEvaluator,
+    FaithfulnessEvaluator,
+    PairwiseComparisonEvaluator,
 )
 
-from llama_index.node_parser import SentenceWindowNodeParser
-from llama_index.llms import OpenAI
-from llama_index.embeddings import OpenAIEmbedding
-from llama_index import ServiceContext
-from llama_index import VectorStoreIndex
-from llama_index.indices.postprocessor import MetadataReplacementPostProcessor
-from llama_index.indices.postprocessor import SentenceTransformerRerank
+from llama_index.core.evaluation.eval_utils import (
+    get_responses,
+    get_results_df,
+)
+from llama_index.core.evaluation import BatchEvalRunner
 
+from collections import defaultdict
+import pandas as pd
 
-from decouple import config
+os.environ["OPENAI_API_KEY"] = "sk-..."
 
-os.environ["OPENAI_API_KEY"] = config("OPENAI_API_KEY")
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core.node_parser import SentenceWindowNodeParser
+from llama_index.core.node_parser import SentenceSplitter
 
-
-documents = SimpleDirectoryReader(
-    input_dir="../dataFiles/"
-).load_data(show_progress=True)
-
-
-document = Document(text="\n\n".join([doc.text for doc in documents]))
+from llama_index.core.evaluation.eval_utils import (
+    get_responses,
+    get_results_df,
+)
+from llama_index.core.evaluation import BatchEvalRunner
 
 node_parser = SentenceWindowNodeParser.from_defaults(
     window_size=3,
@@ -33,42 +49,124 @@ node_parser = SentenceWindowNodeParser.from_defaults(
     original_text_metadata_key="original_text",
 )
 
+text_splitter = SentenceSplitter()
+
 llm = OpenAI(model="gpt-3.5-turbo", temperature=0.1)
-embed_model = OpenAIEmbedding()
-
-sentence_context = ServiceContext.from_defaults(
-    llm=llm,
-    embed_model=embed_model,
-    node_parser=node_parser,
+embed_model = HuggingFaceEmbedding(
+    model_name="sentence-transformers/all-mpnet-base-v2", max_length=512
 )
 
-if not os.path.exists("./storage"):
-    index = VectorStoreIndex.from_documents(
-        [document], service_context=sentence_context
-    )
+Settings.llm = llm
+Settings.embed_model = embed_model
+Settings.text_splitter = text_splitter
 
-    index.storage_context.persist(persist_dir="./storage")
-else:
-    index = load_index_from_storage(
-        StorageContext.from_defaults(persist_dir="./storage"),
-        service_context=sentence_context
-    )
+documents = SimpleDirectoryReader(
+    input_files=["./IPCC_AR6_WGII_Chapter03.pdf"]
+).load_data()
 
+nodes = node_parser.get_nodes_from_documents(documents)
 
-postproc = MetadataReplacementPostProcessor(
-    target_metadata_key="window"
+base_nodes = text_splitter.get_nodes_from_documents(documents)
+
+sentence_index = VectorStoreIndex(nodes)
+
+base_index = VectorStoreIndex(base_nodes)
+
+query_engine = sentence_index.as_query_engine(
+    similarity_top_k=2,
+    node_postprocessors=[
+        MetadataReplacementPostProcessor(target_metadata_key="window")
+    ],
+)
+window_response = query_engine.query(
+    "What are the concerns surrounding the AMOC?"
+)
+print(window_response)
+
+window = window_response.source_nodes[0].node.metadata["window"]
+sentence = window_response.source_nodes[0].node.metadata["original_text"]
+
+print(f"Window: {window}")
+print("------------------")
+print(f"Original Sentence: {sentence}")
+
+for source_node in window_response.source_nodes:
+    print(source_node.node.metadata["original_text"])
+    print("--------")
+
+nest_asyncio.apply()
+
+len(base_nodes)
+
+num_nodes_eval = 30
+sample_eval_nodes = random.sample(base_nodes[:200], num_nodes_eval)
+
+dataset_generator = DatasetGenerator(
+    sample_eval_nodes,
+    llm=OpenAI(model="gpt-4"),
+    show_progress=True,
+    num_questions_per_chunk=2,
 )
 
-rerank = SentenceTransformerRerank(
-    top_n=2, model="BAAI/bge-reranker-base"
+eval_dataset = await dataset_generator.agenerate_dataset_from_nodes()
+
+eval_dataset.save_json("data/ipcc_eval_qr_dataset.json")
+
+eval_dataset = QueryResponseDataset.from_json("data/ipcc_eval_qr_dataset.json")
+
+evaluator_c = CorrectnessEvaluator(llm=OpenAI(model="gpt-4"))
+evaluator_s = SemanticSimilarityEvaluator()
+evaluator_r = RelevancyEvaluator(llm=OpenAI(model="gpt-4"))
+evaluator_f = FaithfulnessEvaluator(llm=OpenAI(model="gpt-4"))
+
+max_samples = 30
+
+eval_qs = eval_dataset.questions
+ref_response_strs = [r for (_, r) in eval_dataset.qr_pairs]
+
+base_query_engine = base_index.as_query_engine(similarity_top_k=2)
+
+query_engine = sentence_index.as_query_engine(
+    similarity_top_k=2,
+
+    node_postprocessors=[
+        MetadataReplacementPostProcessor(target_metadata_key="window")
+    ],
 )
 
-sentence_window_engine = index.as_query_engine(
-    similarity_top_k=5, node_postprocessors=[postproc, rerank]
+base_pred_responses = get_responses(
+    eval_qs[:max_samples], base_query_engine, show_progress=True
+)
+pred_responses = get_responses(
+    eval_qs[:max_samples], query_engine, show_progress=True
 )
 
-response = sentence_window_engine.query(
-    "What did the president say about covid-19?"
+pred_response_strs = [str(p) for p in pred_responses]
+base_pred_response_strs = [str(p) for p in base_pred_responses]
+
+evaluator_dict = {
+    "correctness": evaluator_c,
+    "faithfulness": evaluator_f,
+    "relevancy": evaluator_r,
+    "semantic_similarity": evaluator_s,
+}
+batch_runner = BatchEvalRunner(evaluator_dict, workers=2, show_progress=True)
+
+eval_results = await batch_runner.aevaluate_responses(
+    queries=eval_qs[:max_samples],
+    responses=pred_responses[:max_samples],
+    reference=ref_response_strs[:max_samples],
 )
 
-print(response)
+base_eval_results = await batch_runner.aevaluate_responses(
+    queries=eval_qs[:max_samples],
+    responses=base_pred_responses[:max_samples],
+    reference=ref_response_strs[:max_samples],
+)
+
+results_df = get_results_df(
+    [eval_results, base_eval_results],
+    ["Sentence Window Retriever", "Base Retriever"],
+    ["correctness", "relevancy", "faithfulness", "semantic_similarity"],
+)
+display(results_df)
